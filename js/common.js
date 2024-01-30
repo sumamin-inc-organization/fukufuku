c177592d5ddb9b3e72345c6901b1d7c0baaeb3c7
ShomaKumagawa <s.kumagawa@sumamin.com>
2024-01-30 23:16:58 +0900

    slick追加、バナートグル追加

--- js/common.js
+++ js/common.js
@@ -5,4 +5,19 @@ $(function() {
       $(this).find('.faq-button').toggleClass('open');
       $(this).next().slideToggle();
     });
+
+    $(".kv").slick({
+      autoplay: true, // 自動再生
+      autoplaySpeed: 3000, // 再生速度（ミリ秒設定） 1000ミリ秒=1秒
+      infinite: true, // 無限スライド
+      arrows: false, // 矢印
+      dots: false, // インジケーター
+      pauseOnFocus: false,//フォーカスで一時停止
+      pauseOnHover: false,//マウスホバーで一時停止
+    });
+
+    $('.banner-03-button').click(function() {
+      $(this).find('.banner-icon').toggleClass('open');
+      $('.banner-03-clidren').slideToggle();
+    });
   });
